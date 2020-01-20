a42e3b06156e11b74168142541d9deedf38c044f
Brad <bradley.hayes@tithe.ly>
2020-01-20 22:31:24 +1100

    wip

--- php/CLI.php
+++ php/CLI.php
@@ -1,16 +1,22 @@
 <?php
 
-
-
 namespace BHayes\CLI;
-
-
 
 class CLI
 {
-    const NO_VIS = "was declared";
-    
-    function bad_fucntion(){
-        return 'something';
+    /**
+     * @var object
+     */
+    private $class;
+
+
+    /**
+     * CLI constructor.
+     * @param object|null $class if unspecified CLI will use itself as the subject
+     */
+    public function __construct(object $class = null)
+    {
+        $this->class = $class ??  $this;
+        print_r($this->class);
     }
 }
