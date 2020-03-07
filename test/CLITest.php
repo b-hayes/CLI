fb22855d82252474e7e099d4ebdb642e497ad4d9
b-hayes <bradley.hayes@tithe.ly>
2020-03-07 18:40:43 +1100

    test default values and empty strings work with prompt

--- test/CLITest.php
+++ test/CLITest.php
@@ -33,19 +33,37 @@ class CLITest extends TestCase
     }
 
     /**
-     * Test the different behaviours of the prompt command with a replacement input stream
+     * Test the different behaviours of the prompt command with a replacement input stream.
+     * Note: there is an expectOutput() function in phpunit but it only works once.
      */
     public function testPrompt()
     {
         $prompt = 'enter your name';
-        $this->setInput('bill');
 
         //test that the prompt and the return value of prompt is correct
+        $this->setInput('bill');
         ob_start();
         $input = $this->cli->prompt($prompt);
         $output = ob_get_clean();
         self::assertEquals($prompt, $output);
         self::assertEquals('bill', $input);
+
+        //test that an empty string can be received
+        $this->setInput('');
+        ob_start();
+        $input = $this->cli->prompt($prompt);
+        $output = ob_get_clean();
+        self::assertEquals($prompt, $output);
+        self::assertEquals('', $input);
+
+        //test that a default value will be returned from a return
+        $this->setInput("\n");
+        ob_start();
+        $defaultValue = 'a default value';
+        $input = $this->cli->prompt($prompt, $defaultValue);
+        $output = ob_get_clean();
+        self::assertEquals("$prompt [$defaultValue]", $output);
+        self::assertEquals($defaultValue, $input);
     }
 
     public function testRun()
